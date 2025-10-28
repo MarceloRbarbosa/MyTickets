1b76d8078679326d8e3c219a1ac1cf9201e4fde9
unknown <marceloribeirobarbosa92@gmail.com>
2025-10-28 09:57:18 -0300

    feat(test):adicionar base para testes com Jest

---
+++ src/tests/events.test.ts
@@ -0,0 +1,13 @@
+import supertest from "supertest";
+import app from "../index"
+
+const api = supertest(app)
+
+
+describe("CRUD events", () => {
+    
+    
+    it("should return all events", async () => {
+        
+    });
+});
