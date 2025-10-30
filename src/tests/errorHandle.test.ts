98677b87f5fe1d50724b12fd5fce18da84e648eb
unknown <marceloribeirobarbosa92@gmail.com>
2025-10-29 22:39:39 -0300

    feat(test): adicionar rota de teste para error Handle Middleware

--- src/tests/errorHandle.test.ts
+++ src/tests/errorHandle.test.ts
@@ -1,11 +1,30 @@
-import supertest from "supertest";  
-import httpStatus from "http-status";
+import express, { Request, Response, NextFunction } from "express";
+import request from "supertest";
+import errorHandlerMiddleware, { ERRORS } from "../middlewares/error-middleware";
 
-import app from "../index";
+describe("Middleware: errorHandlerMiddleware", () => {
+  let app: express.Express;
+
+  beforeAll(() => {
+    app = express();
+
+    app.get("/simulate/:type", (req: Request, res: Response, next: NextFunction) => {
+      const type = req.params.type;
+      const err: any = { type, message: `Erro do tipo ${type}` };
+      next(err);
+    });
 
 
-const api = supertest(app)
+    app.use(errorHandlerMiddleware);
+  });
 
-describe("ErrorHandlerMiddleware", () => {
-    
-})
+  it("should return status and message correctly for known error types",async () => {
+    for (const [type, statusCode] of Object.entries(ERRORS)) {
+      const response = await request(app).get(`/simulate/${type}`);
+      expect(response.status).toBe(statusCode);
+      expect(response.text).toBe(`Erro do tipo ${type}`);
+    }
+  });
+
+
+});
